7672969e31e35262e4509e76c68b4124af337e13
Kenji Funayama <kenji.funayama@gmail.com>
2013-03-25 17:32:02 +0900

    シンボリックリンクスクリプト追加

---
+++ make_link.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+ln -s ~/DesktopProfile/.zshrc ~/.zshrc
+ln -s ~/DesktopProfile/.zsh_profile ~/.zsh_profile
+ln -s ~/DesktopProfile/.exrc ~/.exrc
+ln -s ~/DesktopProfile/.vimrc ~/.vimrc
+ln -s ~/DesktopProfile/.gvimrc ~/.gvimrc
+ln -s ~/DesktopProfile/vimfiles ~/.vim
+ln -s ~/DesktopProfile/.vimperatorrc ~/.vimperatorrc
+ln -s ~/DesktopProfile/vimperator ~/.vimperator
