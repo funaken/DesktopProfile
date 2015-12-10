fdb317bba6208411933083322f18fee4dbd6a705
Kenji Funayama <kenji.funayama@gmail.com>
2015-12-10 12:22:08 +0900

    Add git_config_alias.sh

---
+++ script/git_config_alias.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if ! type git > /dev/null; then
+  echo "git not found"
+  exit 1
+fi
+
+git config --global alias.co checkout
+git config --global alias.st 'status'
+git config --global alias.br 'branch'
+git config --global alias.pul 'pull --rebase'
+git config --global alias.fet 'fetch'
+git config --global alias.sta 'stash --include-untracked'
+git config --global alias.stap 'stash pop'
+git config --global alias.rom 'rebase origin/master'
+
